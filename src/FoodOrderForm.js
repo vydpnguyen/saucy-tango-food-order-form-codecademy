0e45281c57185f09e209503163b0dbbc809a07ac
Vy Nguyen <vydpnguyen@gmail.com>
2023-07-23 22:30:21 +0700

    Add event handler for each input field

--- src/FoodOrderForm.js
+++ src/FoodOrderForm.js
@@ -4,21 +4,21 @@ function FoodOrderForm() {
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
     const [address, setAddress] = useState('');
-    const [order, submitOrder] = useState('');
+    const [order, setOrder] = useState('');
 
     return(
         <form>
             <label htmlFor='name'>Name:</label>
-            <input id='name' value={name}></input>
+            <input id='name' value={name} onChange={(e) => setName(e.target.value)}></input>
             <br/>
             <label htmlFor='phone'>Phone:</label>
-            <input id='phone' value={phone}></input>
+            <input id='phone' value={phone} onChange={(e) => setPhone(e.target.value)}></input>
             <br/>
             <label htmlFor='address'>Address:</label>
-            <input id='address' value={address}></input>
+            <input id='address' value={address} onChange={(e) => setAddress(e.target.value)}></input>
             <br/>
             <label htmlFor='order'>Order:</label>
-            <input id='order' value={order}></input>
+            <input id='order' value={order} onChange={(e) => setOrder(e.target.value)}></input>
             <br/>
             <button type='submit'>Submit Order</button>
         </form>
