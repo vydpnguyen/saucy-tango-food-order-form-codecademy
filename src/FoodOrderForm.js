64cb73be280bac723cba7e44bbda06454687df5f
Vy Nguyen <vydpnguyen@gmail.com>
2023-07-23 22:26:33 +0700

    Define state variable and state setters for input data

--- src/FoodOrderForm.js
+++ src/FoodOrderForm.js
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 
 function FoodOrderForm() {
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+    const [order, submitOrder] = useState('');
+
     return(
         <form>
             <label htmlFor='name'>Name:</label>
             <input id='name'></input>
+            <br/>
             <label htmlFor='phone'>Phone:</label>
             <input id='phone'></input>
-            <label htmlFor='addres'>Address:</label>
+            <br/>
+            <label htmlFor='address'>Address:</label>
             <input id='address'></input>
+            <br/>
             <label htmlFor='order'>Order:</label>
             <input id='order'></input>
+            <br/>
             <button type='submit'>Submit Order</button>
         </form>
     );
